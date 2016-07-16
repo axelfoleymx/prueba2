e7a62adc2d68f1d1f1ba9a5e2bea524729053730
¨axelfoleymx¨ <a01164173@gmail.com>
2016-07-15 22:06:29 -0500

    bien p1

--- p1/main.js
+++ p1/main.js
@@ -1,12 +1,8 @@
-var valor1;
-var valor2;
 
-
-valor1 =prompt("Ingresa Numero");
-valor2 =prompt("Ingresa Numero");
 
 do{
-
+var valor1 =prompt("Ingresa Numero");
+var valor2 =prompt("Ingresa Numero");
 suma= parseInt(valor1 +valor2);
 
 document.write(suma);
